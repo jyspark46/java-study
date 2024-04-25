018ae00d564e7c0b47c37d62f1960c7e9f26a8a2
jelypvid <jyspark46@gmail.com>
2024-04-25 17:27:02 +0900

    chapter03 #07

---
+++ chapter03/src/main/java/paint/Point.java
@@ -0,0 +1,43 @@
+package paint;
+
+public class Point implements Drawable{
+	private int x;
+	private int y;
+	
+//	public Point() {
+//		
+//	}
+	
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public void draw(boolean visible) {
+		if(visible) {
+			draw();
+		} else {
+			System.out.println("점(" + x + ", " + y + ")을 지웠습니다.");
+		}
+		
+	}
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("점(" + x + ", " + y + ")을 그렸습니다.");
+	}
+}
