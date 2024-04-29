8043cfcace17ef7d3b4f7c0fedf48c98f4a10c9e
jelypvid <jyspark46@gmail.com>
2024-04-29 14:14:18 +0900

    chpater04 #06

---
+++ Chapter04/src/main/java/chpater04/CalendarTest.java
@@ -0,0 +1,23 @@
+package chpater04;
+
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.TimeZone;
+
+public class CalendarTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// new Calendar(); --> abstract 이므로 객체 생성 불가
+		// -->
+		Calendar cd = Calendar.getInstance();
+		
+//		Locale aLocale = Locale.getDefault(Locale.Category.FORMAT);
+//		TimeZone tz = TimeZone.getDefault();
+//		System.out.println(aLocale + ":" + tz);
+		
+		
+
+	}
+
+}
