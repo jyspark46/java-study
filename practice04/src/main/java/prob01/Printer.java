acba88d609a36b4bd24a8870dc681b1a509c3e6f
jelypvid <jyspark46@gmail.com>
2024-04-29 15:42:24 +0900

    practice04 #03

--- practice04/src/main/java/prob01/Printer.java
+++ practice04/src/main/java/prob01/Printer.java
@@ -2,24 +2,52 @@ package prob01;
 
 public class Printer {
 
-	public void println(int intNum) {
-		// TODO Auto-generated method stub
-		System.out.println(intNum);
+//	public void println(int intNum) {
+//		// TODO Auto-generated method stub
+//		System.out.println(intNum);
+//	}
+//	
+//	public void println(boolean bool) {
+//		// TODO Auto-generated method stub
+//		System.out.println(bool);
+//	}
+//	
+//	public void println(double doubleNum) {
+//		// TODO Auto-generated method stub
+//		System.out.println(doubleNum);
+//	}
+//	
+//	public void println(String str) {
+//		// TODO Auto-generated method stub
+//		System.out.println(str);
+//	}
+	
+	/*// Generic 예시 !!! --> int i = println("hello", 1);
+	public <T, S> T println(T t, S s) {
+		System.out.println(t);
+		return t;
+	}*/
+	
+	// Generic 예시 !!! --> Overloading 안해도 됨 !!
+	public <T> void println(T t) {
+		System.out.println(t);
 	}
 	
-	public void println(boolean bool) {
+	public <T> void println(T... ts) {
+		for(T t : ts) {
+			System.out.println(t);
+		}
+	}
+
+	public int sum(Integer... nums) {
 		// TODO Auto-generated method stub
-		System.out.println(bool);
+		int s = 0;
+		for(Integer n : nums) {
+			s += n;
+		}
+		
+		return s;
 	}
 	
-	public void println(double doubleNum) {
-		// TODO Auto-generated method stub
-		System.out.println(doubleNum);
-	}
-	
-	public void println(String str) {
-		// TODO Auto-generated method stub
-		System.out.println(str);
-	}
 
 }
--- practice04/src/main/java/prob01/PrinterTest.java
+++ practice04/src/main/java/prob01/PrinterTest.java
@@ -5,9 +5,17 @@ public class PrinterTest {
 	public static void main(String[] args) {
 		Printer printer = new Printer();
 
-		printer.println( 10 );
+		printer.<Integer>println( 10 ); // < > 생략 가능 !!
 		printer.println( true );
 		printer.println( 5.7 );
 		printer.println( "홍길동" );
+		
+		printer.println( 10, "홍길동" );
+		printer.println( 10, true, "홍길동" );
+		
+		System.out.println(printer.sum(1));
+		System.out.println(printer.sum(1, 2, 3));
+		System.out.println(printer.sum(1, 2, 3, 4, 5));
+		System.out.println(printer.sum(1, 2, 3, 4, 5, 6, 7, 8));
 	}
 }
