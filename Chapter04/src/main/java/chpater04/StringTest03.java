c1ea1744ff5eb7d69258e13dd5d607ef9e2767bf
jelypvid <jyspark46@gmail.com>
2024-04-29 09:52:31 +0900

    chapter04 #04

--- Chapter04/src/main/java/chpater04/StringTest03.java
+++ Chapter04/src/main/java/chpater04/StringTest03.java
@@ -12,8 +12,28 @@ public class StringTest03 {
 					.append(17)
 					.toString();
 		
+		String s2 = new StringBuilder("Hello ")
+				.append("World")
+				.append(" java")
+				.append(17)
+				.toString();
+		
 		System.out.println(s1);
-
+		System.out.println(s2);
+		
+		String s3 = "";
+		for(int i = 0; i < 1000000; i++) {
+			//s3 += "h";
+			//s3 = new StringBuffer(s3).append("h".toString());
+		}
+		
+		StringBuffer sb = new StringBuffer("");
+		for(int i = 0; i < 1000000; i++) {
+			//s3 += "h";
+			sb.append("h");
+		}
+		String s4 = sb.toString();
+		
 	}
 
 }
