b6bae13fc0eac522a3104d2e8acdbf5acf354efa
jelypvid <jyspark46@gmail.com>
2024-04-29 10:39:41 +0900

    chapter04 #05

--- Chapter04/src/main/java/chpater04/StringTest03.java
+++ Chapter04/src/main/java/chpater04/StringTest03.java
@@ -21,7 +21,7 @@ public class StringTest03 {
 		System.out.println(s1);
 		System.out.println(s2);
 		
-		String s3 = "";
+		//String s3 = "";
 		for(int i = 0; i < 1000000; i++) {
 			//s3 += "h";
 			//s3 = new StringBuffer(s3).append("h".toString());
@@ -32,8 +32,35 @@ public class StringTest03 {
 			//s3 += "h";
 			sb.append("h");
 		}
-		String s4 = sb.toString();
+		String s3 = sb.toString();
 		
+		// String methods
+		String s4 = "aBcABCabcAbc";
+		System.out.println(s4.length());
+		System.out.println(s4.charAt(2));
+		System.out.println(s4.indexOf("abc"));
+		System.out.println(s4.indexOf("abc", 7));
+		System.out.println(s4.substring(3));
+		System.out.println(s4.substring(3, 5)); // (a, b): a ~ b 직전		
+		
+		String s5 = "       ab         cd   ";
+		String s6 = "efg,hij,klm,nop,qrs";
+				
+		String s7 = s5.concat(s6);
+		System.out.println(s7);
+		
+		System.out.println("----" + s5.trim() + "----"); // 겉에 있는 공백 제거
+		System.out.println("----" + s5.replaceAll(" ", "") + "----"); // 모든 공백 제거
+		
+		String[] tokens = s6.split(",");
+		for(String s : tokens) {
+			System.out.println(s);
+		}
+		
+		String[] tokens2 = s6.split(" ");
+		for(String s : tokens2) { 
+			System.out.println(s);
+		}
 	}
 
 }
