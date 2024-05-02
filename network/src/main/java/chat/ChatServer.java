d33f3981761a783e1bccab978db7b7df9e652c66
jelypvid <jyspark46@gmail.com>
2024-05-02 16:16:00 +0900

    network #chat

---
+++ network/src/main/java/chat/ChatServer.java
@@ -0,0 +1,11 @@
+package chat;
+
+public class ChatServer {
+
+	public static void main(String[] args) {
+		
+		// list 부터 만들기
+
+	}
+
+}
---
+++ network/src/main/java/chat/ChatServerThread.java
@@ -0,0 +1,20 @@
+package chat;
+
+public class ChatServerThread extends Thread {
+	
+	private String nickName;
+
+	@Override
+	public void run() {
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+}
